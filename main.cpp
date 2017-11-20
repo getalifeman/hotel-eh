98e7377ff79442b649a1eeb96ccd335933520c7e
Gaurav <32373660+getalifeman@users.noreply.github.com>
2017-11-20 18:25:17 +0300

    Added login function and fixed some issues

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 int r1=0,i1=0; //global variables
 
+void menu();
+
 //////// function to produce delay ////////
 
 void delay()
@@ -41,8 +43,150 @@ void box(int x, int y, int l, int b, char ch)
  }
 }
 
+/* class defintion
+		for password management */
+
+class pwd_mng
+{
+ private:
+ char usn[30];
+ char pwd[30];
+ public:
+
+ void create_acc();
+
+ char* getuser()
+ {
+  return usn;
+ }
+
+ char* getpwd()
+ {
+  return pwd;
+ }
+};
+
+//////// function to create an account ////////////
+
+void pwd_mng :: create_acc()
+{
+ gotoxy(20,10);
+ cout<<"Create Username:";
+ gets(usn);
+ gotoxy(20,12);
+ cout<<"Create Password:";
+ gotoxy(36,12);
+ int x=35;
+ int i=0;
+ char ch;
+ ch = getch();
+ n: if(ch != 13)
+ {
+  ++x;
+  if(ch==8)
+  {
+	pwd[i-1]=pwd[i];
+	--i;
+	gotoxy(x-1,12);
+	cout<<" ";
+	gotoxy(x-1,12);
+	ch=getch();
+  }
+  pwd[i]=ch;
+  cout << '*';
+  ch=getch();
+  ++i;
+  goto n;
+ }
+}
+
+////////// function for login ////////////
+
+void login()
+{
+ again:
+ clrscr();
+ box(0,0,80,26,'*');
+ char user[30], pass[30];
+ pwd_mng p;
+ ifstream fin;
+ fin.open("pwd.dat",ios::in|ios::binary);
+ gotoxy(20,10);
+ cout<<"Username:";
+ gets(user);
+ gotoxy(20,12);
+ cout<<"Password:";
+ gotoxy(29,12);
+ int x=28;
+ int i=0;
+ char ch;
+ ch = getch();
+ n: if(ch != 13)
+ {
+	++x;
+	if(ch==8)
+	{
+	 pass[i-1]=pass[i];
+	 --i;
+	 gotoxy(x-1,12);
+	 cout<<" ";
+	 gotoxy(x-1,12);
+	 ch=getch();
+	}
+ pass[i]=ch;
+ cout << '*';
+ ch=getch();
+ ++i;
+ goto n;
+}
+ while(fin.read((char*)&p,sizeof(p)))
+ {
+  if( strcmp(p.getuser(),user)==0 && strcmp(p.getpwd(),pass)==0 )
+	{
+	 gotoxy(38,30);
+	 cout<<"Access granted";
+	 menu();
+	}
+  else
+  {
+	gotoxy(38,20);
+	cout<<"Access revoked... Try again!";
+	delay();
+	goto again;
+  }
+ }
+ fin.close();
+}
+
+///////////// start program function //////////////
+
+void start_prgm()
+{
+ clrscr();
+ box(0,0,80,26,'*');
+ pwd_mng P;
+ ofstream fout;
+ ifstream fin;
+ fin.open("pwd.dat",ios::in|ios::binary);
+ fout.open("temp.dat",ios::app|ios::binary);
+ if(!fin)
+ {
+  P.create_acc();
+  fout.write((char*)&P,sizeof(P));
+  fout.close();
+  rename("temp.dat","pwd.dat");
+  exit(0);
+ }
+ else
+ {
+  fin.close();
+  login();
+ }
+}
+
+
 /*class
-		defintion for customer details */
+	defintion for customer details */
 
 class guest
 {
@@ -206,8 +350,12 @@ void append()
 	fout.close();
   }
   else
-	cout<<"Room number or ID number already assigned.. Try Again\n";
-	delay();
+	{
+	 box(0,0,80,26,'*');
+	 gotoxy(11,11);
+	 cout<<"Room number or ID number already assigned.. Try Again\n";
+	 delay();
+	}
  }
  return;
 }
@@ -336,9 +484,9 @@ void search()
 		  cout<<"Thank You for using the program!";
 		  exit(0);
 		  break;
-	         }
+		}
 	default:clrscr();
-	        break;
+			 break;
   }
  goto start;
 }
@@ -347,5 +495,5 @@ void search()
 
 void main()
 {
- menu();
+ start_prgm();
 }
